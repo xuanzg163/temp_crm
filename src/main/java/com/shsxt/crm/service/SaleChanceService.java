df8242759115434219d0a1183893996916a9258c
xuanzg163 <xuan_zg@163.com>
2018-11-05 16:32:02 +0800

    1、完成营销机会管理数据更新后端部分，实现用户更新营销机会功能

--- src/main/java/com/shsxt/crm/service/SaleChanceService.java
+++ src/main/java/com/shsxt/crm/service/SaleChanceService.java
@@ -78,6 +78,8 @@ public class SaleChanceService extends BaseService<SaleChance> {
 
         } else {
             //更新
+            AssertUtil.isTrue(saleChanceMapper.update(saleChance) < 1,
+                    CrmConstant.OPS_FAILED_MSG);
         }
 
     }
