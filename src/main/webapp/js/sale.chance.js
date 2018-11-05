711071c558c2bab263387011bce00506da21c413
xuanzg163 <xuan_zg@163.com>
2018-11-05 11:06:26 +0800

    1、格式化营销机会管理显示信息

--- src/main/webapp/js/sale.chance.js
+++ src/main/webapp/js/sale.chance.js
@@ -7,3 +7,50 @@ function querySaleChancesByParams() {
         createDate: $('#time').datebox('getValue')
     });
 }
+
+//分配状态-数据格式化显示
+function formatterState(value,row,index) {
+    if (0 == value){
+        return "未分配";
+    }
+    if (1 == value){
+        return "已分配";
+    }
+}
+
+//客户开发状态-数据格式化显示
+function formatterDevResult(value,row,index) {
+    if (0 == value){
+        return "未开发";
+    } else if (1 == value){
+        return "开发中";
+    } else if (2 == value){
+        return "开发成功";
+    } else if (3 == value){
+        return "开发失败";
+    }
+}
+
+//修改显示背景颜色,页面加载完毕触发
+$(function () {
+
+    $("#dg").datagrid({
+        rowStyler:function (index,row) {
+            var devResult = row.devResult;
+            if (devResult == 0) {
+                return "background-color:#5bc0de;"; // 蓝色
+            } else if (devResult == 1) {
+                return "background-color:#f0ad4e;"; // 黄色
+            } else if (devResult == 2) {
+                return "background-color:#5cb85c;"; // 绿色
+            } else if (devResult == 3) {
+                return "background-color:#d9534f;"; // 红色
+            }
+        }
+    });
+});
+
+//添加营销机会
+function openAddSaleChacneDialog () {
+
+}
